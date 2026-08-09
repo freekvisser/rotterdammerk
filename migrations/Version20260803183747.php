7a622465e6f2f3ca5a431d03d09c18e027b1376a
Freek Visser <freek@visser.fo>
2026-08-09 10:38:07 +0200

    migrate project to use Docker

--- migrations/Version20260803183747.php
+++ migrations/Version20260803183747.php
@@ -19,23 +19,15 @@ final class Version20260803183747 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('DROP TABLE stock');
-        $this->addSql('ALTER TABLE product DROP FOREIGN KEY `FK_stock_id`');
-        $this->addSql('DROP INDEX FK_stock_id_idx ON product');
-        $this->addSql('DROP INDEX id_UNIQUE ON product');
-        $this->addSql('ALTER TABLE product ADD stock_id CHAR(36) NOT NULL, DROP stockid, CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE name name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE IF NOT EXISTS messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE IF NOT EXISTS product (id CHAR(36) NOT NULL, name VARCHAR(256) DEFAULT NULL, description VARCHAR(1024) DEFAULT NULL, PRIMARY KEY (id), UNIQUE KEY id_UNIQUE (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci`');
+        $this->addSql('CREATE TABLE IF NOT EXISTS stock (product_id CHAR(36) NOT NULL, amount INT DEFAULT NULL, PRIMARY KEY (product_id), UNIQUE KEY id_UNIQUE (product_id), CONSTRAINT FK_product_id FOREIGN KEY (product_id) REFERENCES product (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci`');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE stock (id VARCHAR(45) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, stock INT DEFAULT NULL, UNIQUE INDEX id_UNIQUE (id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('DROP TABLE messenger_messages');
-        $this->addSql('ALTER TABLE product ADD stockid VARCHAR(45) DEFAULT NULL, DROP stock_id, CHANGE id id VARCHAR(45) NOT NULL, CHANGE name name VARCHAR(256) DEFAULT NULL');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT `FK_stock_id` FOREIGN KEY (stockid) REFERENCES stock (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('CREATE INDEX FK_stock_id_idx ON product (stockid)');
-        $this->addSql('CREATE UNIQUE INDEX id_UNIQUE ON product (id)');
+        $this->addSql('DROP TABLE IF EXISTS stock');
+        $this->addSql('DROP TABLE IF EXISTS product');
+        $this->addSql('DROP TABLE IF EXISTS messenger_messages');
     }
 }
